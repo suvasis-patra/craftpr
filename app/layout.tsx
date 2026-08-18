b89b2f483dc10d7e73fc44fda464b6fd923484e7
suvasis-patra <suvasis02@kgpian.iitkgp.ac.in>
2026-08-18 13:18:29 +0530

    setup neon database with prisma

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, JetBrains_Mono, Merriweather } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const merriweatherHeading = Merriweather({subsets:['latin'],variable:'--font-heading'});
+
+const jetbrainsMono = JetBrains_Mono({subsets:['latin'],variable:'--font-mono'});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,7 +26,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-mono", jetbrainsMono.variable, merriweatherHeading.variable)}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
